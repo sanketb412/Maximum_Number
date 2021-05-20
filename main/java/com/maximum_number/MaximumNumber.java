74a5fe680c9b074aefb648bf45c22963ca53815f
sanketb412 <sanketbagde38@gmail.com>
2021-05-20 23:07:07 +0530

    Added: code for finding maximum

--- main/java/com/maximum_number/MaximumNumber.java
+++ main/java/com/maximum_number/MaximumNumber.java
@@ -1,6 +1,17 @@
 package com.maximum_number;
 
 public class MaximumNumber {
+    public Integer maximumNUmber(Integer x, Integer y, Integer z) {
+        Integer maximum = x;               //Initializing maximumNumber
+        if (y.compareTo(maximum) > 0) {
+            maximum = y;
+        }
+        if (z.compareTo(maximum) > 0) {
+            maximum = z;
+        }
+        return maximum;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Test Maximum of Three Variables");
     }
--- test/java/com/maximum_number/MaximumNumberTest.java
+++ test/java/com/maximum_number/MaximumNumberTest.java
@@ -5,4 +5,10 @@ import org.junit.jupiter.api.Test;
 
 public class MaximumNumberTest {
     MaximumNumber maximum = new MaximumNumber();
+
+    @Test
+    public void given3NumberReturnFirstNumber() {
+        Integer result = maximum.maximumNUmber(8,1,45);
+        Assertions.assertEquals(45, result);
+    }
 }
