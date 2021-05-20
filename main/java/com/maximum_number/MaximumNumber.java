e22da2bf41b9ca8e4a93c887e2760b3b206c1dd3
sanketb412 <sanketbagde38@gmail.com>
2021-05-20 23:00:57 +0530

    Added: code for welcoming message

---
+++ main/java/com/maximum_number/MaximumNumber.java
@@ -0,0 +1,7 @@
+package com.maximum_number;
+
+public class MaximumNumber {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Test Maximum of Three Variables");
+    }
+}
---
+++ test/java/com/maximum_number/MaximumNumberTest.java
@@ -0,0 +1,8 @@
+package com.maximum_number;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MaximumNumberTest {
+    MaximumNumber maximum = new MaximumNumber();
+}
